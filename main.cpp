02c2ee38e43d7358bbf47f422dc7226d5e9f24b6
szoladz <samuelus@op.pl>
2025-06-18 17:23:27 +0200

    SFML Hello World

--- main.cpp
+++ main.cpp
@@ -1,25 +1,41 @@
+#include <SFML/Graphics.hpp>
+#include <cstdlib>
+#include <filesystem>
 #include <iostream>
 
-// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 int main() {
-    // TIP Press <shortcut actionId="RenameElement"/> when your caret is at the
-    // <b>lang</b> variable name to see how CLion can help you rename it.
-    auto lang = "C++";
-    std::cout << "Hello and welcome to " << lang << "!\n";
+  sf::RenderWindow window(sf::VideoMode(800, 600), "Hello World");
 
-    for (int i = 1; i <= 5; i++) {
-        // TIP Press <shortcut actionId="Debug"/> to start debugging your code.
-        // We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/>
-        // breakpoint for you, but you can always add more by pressing
-        // <shortcut actionId="ToggleLineBreakpoint"/>.
-        std::cout << "i = " << i << std::endl;
+  namespace fs = std::filesystem;
+  auto exe_path = fs::current_path();
+
+  sf::Font font;
+  if (!font.loadFromFile((exe_path / "../arial.ttf").string())) {
+    std::cerr << "Failed to load font from " << (exe_path / "../arial.ttf")
+              << "\n";
+    return EXIT_FAILURE;
+  }
+
+  sf::Text text;
+  text.setFont(font);
+  text.setString("Hello, World!");
+  text.setCharacterSize(48);
+  text.setFillColor(sf::Color::White);
+  sf::FloatRect bounds = text.getLocalBounds();
+  text.setOrigin(bounds.width / 2, bounds.height / 2);
+  text.setPosition(800 / 2, 600 / 2);
+
+  while (window.isOpen()) {
+    sf::Event event;
+    while (window.pollEvent(event)) {
+      if (event.type == sf::Event::Closed)
+        window.close();
     }
 
-    return 0;
-}
+    window.clear(sf::Color::Black);
+    window.draw(text);
+    window.display();
+  }
 
-// TIP See CLion help at <a
-// href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>.
-//  Also, you can try interactive lessons for CLion by selecting
-//  'Help | Learn IDE Features' from the main menu.
+  return 0;
+}
